2603e98d90b53b2e29283b97b2a642493e523c55
Oscar Sterland <oscar.sterland@alianza.com>
2025-11-28 11:50:37 +0000

    Format.

--- init.lua
+++ init.lua
@@ -13,11 +13,11 @@ vim.keymap.set("v", "<space>x", ":lua<CR>")
 
 -- Highlight on yank autocmd.
 vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking (copying) text.',
-	group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = 'Highlight when yanking (copying) text.',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 vim.cmd('filetype plugin indent on')
