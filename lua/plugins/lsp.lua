1101cb581e8a5d10b78ec8ab03a00c617eec4f31
Oscar Sterland <oscar.sterland@alianza.com>
2025-12-04 13:43:55 +0000

    Add clangd to list of lsps in use and use default config.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,9 +2,10 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      vim.lsp.enable('basedpyright')
+      vim.lsp.enable('clangd')
       vim.lsp.config('lua_ls', require('lsp.lua_ls'))
       vim.lsp.enable('lua_ls')
-      vim.lsp.enable('basedpyright')
     end,
   }
 }
